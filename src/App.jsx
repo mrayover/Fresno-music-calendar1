2e6203ac1e5283921e8295e1868a462de2a122ce
mrayover <mroveracker@gmail.com>
2025-06-22 13:09:18 -0700

    Add full site files for Fresno Music Calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import CalendarPage from './components/CalendarPage';
-import SubmitEventPage from './components/SubmitEventPage';
-import EventDetailPage from './components/EventDetailPage';
-import AboutPage from './components/AboutPage';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/submit" element={<SubmitEventPage />} />
-        <Route path="/event/:id" element={<EventDetailPage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </Router>
-  );
+export default function App() {
+  return <h1>Welcome to Fresno Music Calendar!</h1>;
 }
-
-export default App;
