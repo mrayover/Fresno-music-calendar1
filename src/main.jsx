4e7c6af053231418d6f3e073a2747929e99afafe
mrayover <mroveracker@gmail.com>
2025-06-24 15:06:38 -0700

    ettt

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,14 @@
-
+// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import EventDetail from "./EventDetail";
-import SubmitEvent from "./SubmitEvent";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/event/:id" element={<EventDetail />} />
-        <Route path="/submit-event" element={<SubmitEvent />} />
-      </Routes>
+      <App />
     </Router>
   </React.StrictMode>
 );
