2e3575c5c8a17d31e2744b76f7f63c2ee321d416
mrayover <mroveracker@gmail.com>
2025-06-23 18:28:32 -0700

    3rg

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import CalendarView from "./CalendarView";
 import EventDetail from "./EventDetail";
 import SubmitEvent from "./SubmitEvent";
-import logo from "/assets/logo.png";
-import "./style.css";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <header className="app-header">
-          <img src={logo} alt="Fresno Music Calendar Logo" className="logo" />
+    <div className="app-container">
+      <header className="app-header">
+        <div className="logo-title">
+          <img src="/logo.png" alt="Fresno Music Calendar Logo" className="logo" />
           <h1 className="site-title">Fresno Music Calendar</h1>
-          <nav>
-            <Link to="/" className="nav-link">Calendar</Link>
-            <Link to="/submit" className="nav-link">Submit Event</Link>
-          </nav>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<CalendarView />} />
-            <Route path="/event/:id" element={<EventDetail />} />
-            <Route path="/submit" element={<SubmitEvent />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+        </div>
+        <nav>
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/submit" className="nav-link">Submit Event</Link>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<CalendarView />} />
+          <Route path="/event/:id" element={<EventDetail />} />
+          <Route path="/submit" element={<SubmitEvent />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
-
-export default App;
