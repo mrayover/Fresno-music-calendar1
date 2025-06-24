66d2a2d1228e908f045952c55cd10a85dc4596fe
mrayover <mroveracker@gmail.com>
2025-06-23 20:55:40 -0700

    edd

--- src/CalendarView.jsx
+++ src/CalendarView.jsx
@@ -1,31 +1,45 @@
 
-import React from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import React, { useState } from "react";
+import { Calendar, Views, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import events from "./eventsData";
 
 const localizer = momentLocalizer(moment);
 
-function CalendarView({ events, onSelectEvent }) {
-  const handleSelectEvent = (event) => {
-    onSelectEvent(event);
+const CalendarView = ({ onSelectEvent }) => {
+  const [view, setView] = useState(Views.MONTH);
+
+  const handleViewChange = (newView) => {
+    setView(newView);
   };
 
   return (
-    <div style={{ height: "calc(100vh - 100px)" }}>
+    <div className="calendar-container">
+      <div className="view-selector">
+        {Object.values(Views).map((v) => (
+          <button
+            key={v}
+            className={view === v ? "active" : ""}
+            onClick={() => handleViewChange(v)}
+          >
+            {v.charAt(0).toUpperCase() + v.slice(1)}
+          </button>
+        ))}
+      </div>
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
-        titleAccessor="title"
-        onSelectEvent={handleSelectEvent}
-        views={['month', 'week', 'day', 'agenda']}
-        defaultView="month"
-        style={{ height: "100%" }}
+        style={{ height: "80vh" }}
+        views={["month", "week", "day", "agenda"]}
+        view={view}
+        onView={handleViewChange}
+        onSelectEvent={onSelectEvent}
       />
     </div>
   );
-}
+};
 
 export default CalendarView;
