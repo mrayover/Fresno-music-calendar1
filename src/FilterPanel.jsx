c8bff4bb8aa532b6de3b7803827bea7bec1a27d5
mrayover <mroveracker@gmail.com>
2025-06-24 18:17:46 -0700

    Update FilterPanel

--- src/FilterPanel.jsx
+++ src/FilterPanel.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 
 const FilterPanel = ({ genres, selectedGenres, onFilterChange }) => {
+  const allSelected = genres.length === selectedGenres.length;
+
+  const handleSelectAll = () => {
+    if (allSelected) {
+      genres.forEach(genre => onFilterChange(genre)); // Deselect all
+    } else {
+      genres
+        .filter(genre => !selectedGenres.includes(genre))
+        .forEach(genre => onFilterChange(genre)); // Select missing ones
+    }
+  };
+
   return (
-    <div className="filter-panel">
+    <div className="filter-panel" style={{ marginBottom: "1rem" }}>
       <h3>Filter by Genre</h3>
-      <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={allSelected}
+          onChange={handleSelectAll}
+        />
+        Select All
+      </label>
+      <div style={{ display: "flex", flexDirection: "column", marginTop: "0.5rem" }}>
         {genres.map((genre) => (
           <label key={genre}>
             <input
