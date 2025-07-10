03a1777ac6709851af405907c52ba9b9bcf4733f
mrayover <mroveracker@gmail.com>
2025-07-09 21:32:28 -0700

    gsdgsfg

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwind from 'vite-plugin-tailwind'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwind(), // 👈 injects Tailwind v4 support properly
-  ],
-})
+  plugins: [react()],
+});
