27e162fe188584df160a4d75caaa530683ba5c17
mrayover <mroveracker@gmail.com>
2025-07-22 19:40:40 -0700

    nin

--- src/SubmitEventForm.jsx
+++ src/SubmitEventForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
 import EventForm from "./components/EventForm";
 import ReCAPTCHA from "react-google-recaptcha";
